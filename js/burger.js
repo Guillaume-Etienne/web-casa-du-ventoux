8be3af5d1a0b4326dc964bad45530c236b0bbcbc
Gui <gsetienne@free.fr>
2022-11-12 13:07:29 +0200

    texts added

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,5 @@
 // AOS (animation des divs)   / Buger menu    /
-AOS.init(); //oui c'est tout, tout se fait direct dans le html
+// AOS.init(); //oui c'est tout, tout se fait direct dans le html
 
 // burger :
 
@@ -20,25 +20,3 @@ function openNav() {
 function closeNav() {
   sidenav.classList.remove("active");
 }
-
-// language automatised selector
-
-// Identifie and store selected language
-const langSelector = document.getElementById('lang');
-langSelector.addEventListener('click', function(event){
-  // event.preventDefault()
-  console.log('ça a cliqué ! la : '+ event.target)  
-  var resultLang=event.target.toString() 
-
-  if (resultLang.includes("es")){    
-    localStorage.setItem("lang","es")
-  }
-  else if (resultLang.includes("en")){    
-    localStorage.setItem("lang","en")
-  }
-  else{    
-    localStorage.setItem("lang","fr")
-  }
-});
-
-// detect and apply  -> on specific JS to work only on the index
